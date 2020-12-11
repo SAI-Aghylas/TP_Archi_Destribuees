796dedd5623762c3a1c5d65870144a3921af623f
aghylassai <aghilassai@gmail.com>
2020-12-11 11:13:32 +0100

    last try

--- src/main/scala/fr/data/spark/DataFrame.scala
+++ src/main/scala/fr/data/spark/DataFrame.scala
@@ -2,6 +2,7 @@ package fr.data.spark
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.functions.udf
 import fr.data.spark.ReaderCsvFile
 object DataFrame {
 
@@ -10,13 +11,48 @@ object DataFrame {
       .appName("code_postal_tp")
       .master("local[2]")
       .getOrCreate()
+    //reading files:
     val reader= ReaderCsvFile(spark)
     val df_code_insee= reader.readcsv("src/main/resources/code-insee-postaux-geoflar.csv")
     val df_code_insee_columns=reader.storeColumn(df_code_insee)
-    //affichage du nbr de communes:
-    //val c1=df.select("Code_commune_INSEE").distinct().count()
-    //print("nombre de commmunes:"+c1)
-    //afficher le nombre de communes qui ont l'attribut ligne_5
+    //df_code_insee.show()
+    val df_communes = reader.readcsv("src/main/resources/Communes.csv")
+    val df_communes_columns=reader.storeColumn(df_communes)
+    //df_communes.show()
+    val df_postes = reader.readcsv("src/main/resources/postesSynop.txt")
+    val df_postes_columns=reader.storeColumn(df_postes)
+    //df_postes.show()
+    val df_meteo = reader.readcsv("src/main/resources/synop.2020120512.txt")
+    val df_meteo_columns=reader.storeColumn(df_communes)
+    //df_meteo.show()
+
+
+    //begin selection
+    val code_insee=df_code_insee.select("CODE INSEE","Code Dept","geom_x_y")
+    val geom_x_y = df_code_insee.select("geom_x_y")
+    geom_x_y.show()
+    //code_insee.show()
+    val commune=df_communes.select("DEPCOM","PTOT")
+    //commune.show()
+    val postes= df_postes.select("ID","Latitude","Longitude")
+    //postes.show()
+    val udfTemperature=udf((s:Double)=>(s-273.15).toInt)
+    val meteo= df_meteo.withColumn("t",udfTemperature(df_meteo("t").alias("t_k")))
+    //meteo.show()
+    val meteo2 =meteo.select("t","date","numer_sta")
+
+
+    //Jointures:
+    val df_comune_code_insee = code_insee.join(commune,commune("DEPCOM")===code_insee("CODE INSEE"),"left").drop(code_insee("CODE INSEE"))
+    df_comune_code_insee.show()
+
+    val df_postes_meteo2= postes.join(meteo2,meteo2("numer_sta")===postes("ID"),"left").drop(postes("ID"))
+    df_postes_meteo2.show()
+
+    //writing files:
+   // reader.writeParket(df_comune_code_insee,"src/main/resources/comune_insee")
+   // reader.writeParket(df_postes_meteo2,"src/main/resources/postes_meteo")
+
 
   }
 
--- src/main/scala/fr/data/spark/ReaderCsvFile.scala
+++ src/main/scala/fr/data/spark/ReaderCsvFile.scala
@@ -11,6 +11,12 @@ class ReaderCsvFile(spark: SparkSession){
   def storeColumn(df: DataFrame):List[String] ={
     df.columns.toList
   }
+  def  distance (p1_lat: Double, p1_lon: Double, p2_lat: Double, p2_lon: Double) :Double= {
+    scala.math.sqrt(scala.math.pow(p2_lat-p1_lat,2) + scala.math.pow(p2_lon - p1_lon,2))
+  }
+  def writeParket(dataFrame: DataFrame,p:String)={
+    dataFrame.write.parquet(path = p)
+  }
 }
 object ReaderCsvFile{
   def apply(spark: SparkSession): ReaderCsvFile = new ReaderCsvFile(spark)
